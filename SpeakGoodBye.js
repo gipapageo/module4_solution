07984149d0b58bc91681be22fa75a5aca09693cf
gipapageo <73595398+gipapageo@users.noreply.github.com>
2020-11-23 19:22:45 +0200

    Add files via upload

---
+++ SpeakGoodBye.js
@@ -0,0 +1,13 @@
+
+
+(function (window) {
+
+	var byeSpeaker = {};
+
+	byeSpeaker.speak = function(name) {
+	  console.log("Good Bye " + name);
+	}
+		
+	window.byeSpeaker = byeSpeaker;
+	
+})(window); 
